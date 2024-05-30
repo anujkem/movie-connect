ff13f95701151ab473975cfcd4fab9e9b2ced792
Anuj <anujkem7@gmail.com>
2024-05-30 19:22:44 +0530

    final touch

--- src/Contextpage.jsx
+++ src/Contextpage.jsx
@@ -1,39 +1,35 @@
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-//=== google firebase import start ===
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../firebase';
-import { useAuthState } from "react-firebase-hooks/auth"
+// //=== google firebase import start ===
+// import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// import { auth } from '../firebase';
+// import { useAuthState } from "react-firebase-hooks/auth"
 // ===================================
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 const Contextpage = createContext();
 
 export function MovieProvider({ children }) {
-
   const [header, setHeader] = useState("Trending");
-  const [totalPage, setTotalPage] = useState(null)
+  const [totalPage, setTotalPage] = useState(null);
   const [movies, setMovies] = useState([]);
   const [searchedMovies, setSearchedMovies] = useState([]);
   const [trending, setTrending] = useState([]);
   const [upcoming, setUpcoming] = useState([]);
   const [page, setPage] = useState(1);
   const [activegenre, setActiveGenre] = useState(28);
-  const [genres, setGenres] = useState([])
+  const [genres, setGenres] = useState([]);
   const [loader, setLoader] = useState(true);
   const [backgenre, setBackGenre] = useState(false);
-  const [user, setUser] = useAuthState(auth)//=======> firebase custom hooks state
-  const navigate = useNavigate();// =====> navigate page
+  const navigate = useNavigate(); // =====> navigate page
 
   const APIKEY = import.meta.env.VITE_API_KEY;
 
-
   useEffect(() => {
     if (page < 1) {
-      setPage(1)  // Increment page to 1 if it is less than 1.
+      setPage(1); // Increment page to 1 if it is less than 1.
     }
   }, [page]);
-
 
   const filteredGenre = async () => {
     const data = await fetch(
@@ -51,10 +47,10 @@ export function MovieProvider({ children }) {
       `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&with_origin_country=IN&language=en-US&query=${query}&page=1&include_adult=false`
     );
     const searchmovies = await data.json();
-    setSearchedMovies(searchmovies.results); 
+    setSearchedMovies(searchmovies.results);
     setLoader(false);
     setHeader(`Results for "${query}"`);
-  }
+  };
 
   const fetchGenre = async () => {
     const data = await fetch(
@@ -62,7 +58,7 @@ export function MovieProvider({ children }) {
     );
     const gen = await data.json();
     setGenres(gen.genres);
-  }
+  };
 
   const fetchTrending = async () => {
     const data = await fetch(
@@ -73,7 +69,7 @@ export function MovieProvider({ children }) {
     setTotalPage(trend.total_pages);
     setLoader(false);
     setHeader("Trending Movies");
-  }
+  };
 
   const fetchUpcoming = async () => {
     const data = await fetch(
@@ -84,28 +80,14 @@ export function MovieProvider({ children }) {
     setTotalPage(upc.total_pages);
     setLoader(false);
     setHeader("Upcoming Movies");
-  }
+  };
 
   // creat local storage
   const GetFavorite = () => {
     setLoader(false);
     setHeader("Favorite Movies");
-  }
+  };
 
-
-  //<========= firebase Google Authentication ========>
-  const googleProvider = new GoogleAuthProvider();// =====> google auth provide
-
-  const GoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, googleProvider);
-      navigate("/")
-      toast.success("Login successfully");
-    } catch (err) {
-      console.log(err)
-      navigate("/")
-    }
-  }
   // <==========================================================>
 
   return (
@@ -135,14 +117,11 @@ export function MovieProvider({ children }) {
         GetFavorite,
         totalPage,
         searchedMovies,
-        GoogleLogin,
-        user
       }}
     >
       {children}
     </Contextpage.Provider>
   );
-
 }
 
-export default Contextpage
+export default Contextpage;
